5d3a7c7f8bffecd995927774d420b28a319f259a
c4pr1c3 <huangwei.me@cuc.edu.cn>
2019-03-09 10:44:21 +0800

    根据 shellcheck 的提示完善渲染脚本

--- render.sh
+++ render.sh
@@ -8,14 +8,11 @@ css_min_file="reveal.js/css/reveal.min.css"
 js_src_file="${parent_dir}/reveal.js/js/reveal.js"
 js_min_file="reveal.js/js/reveal.min.js"
 
-input_files=($(ls *.md))
-
-if [ ! -f ${css_src_file} ] || [ ! -f ${js_src_file} ];then
+if [ ! -f "${css_src_file}" ] || [ ! -f "${js_src_file}" ];then
   # download reveal.js repo
   git_tmp=$(mktemp -d)
   echo "git cloning ${git_repo_url}"
-  git --git-dir="${git_tmp}" clone --depth=1 ${git_repo_url} 
-  if [[ $? -eq 0 ]];then
+  if [[ $(git --git-dir="${git_tmp}" clone --depth=1 ${git_repo_url}) ]];then
     echo "Done with git clone ${git_repo_url}"
   else
     echo "Failed with git clone ${git_repo_url}"
@@ -32,15 +29,19 @@ if [[ ! -e "${js_min_file}" ]];then
   ln -s "${js_src_file}" "${js_min_file}"
 fi
 
-for input in ${input_files[@]};do
-  if [[ -x $(which pandoc) ]];then
+if [[ -x $(command -v pandoc) ]];then
+  for input in ./*.md;do
+    if [[ "$input" == "./README.md" ]];then
+      continue
+    fi
     output_ppt="${input}.html"
     output_html="${input}.print.html"
-    pandoc -t revealjs -s ${input} -V theme=white -V transition=fade -V incremental=true -V slideNumber=true -o "${output_ppt}" -V revealjs-url="${parent_dir}/reveal.js" -V history=true
+    pandoc -t revealjs -s "${input}" -V theme=white -V transition=fade -V incremental=true -V slideNumber=true -o "${output_ppt}" -V revealjs-url="${parent_dir}/reveal.js" -V history=true
     echo "generated presentation file: ${output_ppt}"
-    pandoc -t html5 -s ${input} -V theme=white -V transition=fade -V incremental=true -V slideNumber=true -o "${output_html}" -V revealjs-url="${parent_dir}/reveal.js" -V history=true
+    pandoc -t html5 -s "${input}" -V theme=white -V transition=fade -V incremental=true -V slideNumber=true -o "${output_html}" -V revealjs-url="${parent_dir}/reveal.js" -V history=true
     echo "generated presentation file: ${output_html}"
-  else
-    echo "You need to install pandoc to run this script"
-  fi
-done
+  done
+else
+  echo "You need to install pandoc to run this script"
+  exit 1
+fi
